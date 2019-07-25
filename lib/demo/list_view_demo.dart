d9e518874997a46e765147d49445af325cbc26be
Jentle <juntaozhi@163.com>
2019-07-26 07:08:07 +0800

    添加drawer

---
+++ lib/demo/list_view_demo.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+class ListViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+           itemCount: posts.length,
+           itemBuilder: _listItemBuilder,
+         );
+  }
+
+  Widget _listItemBuilder(BuildContext context, int index){
+
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+           posts[index].title,
+           style: Theme.of(context).textTheme.title,
+        ),
+        Text(
+           posts[index].author,
+           style: Theme.of(context).textTheme.subhead,
+        ),
+        SizedBox(
+            height: 20.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
