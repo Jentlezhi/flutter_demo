6c1baf7ea552b22b2f7773824a95c53866248484
Jentle <zhijt@yiche.com>
2019-07-30 17:55:54 +0800

    listview添加详情页面

--- lib/demo/list_view_demo.dart
+++ lib/demo/list_view_demo.dart
@@ -5,36 +5,64 @@ class ListViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-           itemCount: posts.length,
-           itemBuilder: _listItemBuilder,
-         );
+      itemCount: posts.length,
+      itemBuilder: _listItemBuilder,
+    );
   }
 
-  Widget _listItemBuilder(BuildContext context, int index){
-
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(10.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-           posts[index].title,
-           style: Theme.of(context).textTheme.title,
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+              builder: (BuildContext context) => ListDetail(
+                    post: posts[index],
+                  )),
+        );
+      },
+      child: Container(
+        color: Colors.white,
+        margin: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.network(posts[index].imageUrl),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              posts[index].title,
+              style: Theme.of(context).textTheme.title,
+            ),
+            Text(
+              posts[index].author,
+              style: Theme.of(context).textTheme.subhead,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+          ],
         ),
-        Text(
-           posts[index].author,
-           style: Theme.of(context).textTheme.subhead,
-        ),
-        SizedBox(
-            height: 20.0,
-          ),
-        ],
       ),
+    );
+  }
+}
+
+class ListDetail extends StatelessWidget {
+  final Post post;
+  const ListDetail({@required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          post.title,
+          style: TextStyle(color: Colors.white,fontSize: 20.0),
+        ),
+        elevation: 0.0,
+      ),
+      body: Image.network(post.imageUrl),
     );
   }
 }
