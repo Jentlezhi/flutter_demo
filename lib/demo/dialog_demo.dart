bb51cae2f61df74c7553f9a02455a1c2ecf0bfb6
Jentle <juntaozhi@163.com>
2019-08-04 09:15:54 +0800

    alertDialog的使用

--- lib/demo/dialog_demo.dart
+++ lib/demo/dialog_demo.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'dart:async';
 
 class DialogDemo extends StatefulWidget {
@@ -7,44 +6,43 @@ class DialogDemo extends StatefulWidget {
   _DialogDemoState createState() => _DialogDemoState();
 }
 
-enum Option{
-  A,B,C
-}
+enum Option { A, B, C }
 
 class _DialogDemoState extends State<DialogDemo> {
-
   String _choice = 'Nothing';
-  Future _openSimpleDialog() async{
+  Future _openSimpleDialog() async {
     final option = await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context){
-        return SimpleDialog(
-          title: Text('我是标题',style: TextStyle(fontSize: 14.0),),
-          children: <Widget>[
-            SimpleDialogOption(
-              child: Text('Option A'),
-              onPressed: (){
-                Navigator.pop(context,Option.A);
-              },
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: Text(
+              '我是标题',
+              style: TextStyle(fontSize: 14.0),
             ),
-            SimpleDialogOption(
-              child: Text('Option B'),
-              onPressed: (){
-                debugPrint('onPressed1');
-                Navigator.pop(context,Option.B);
-              },
-            ),
-            SimpleDialogOption(
-              child: Text('Option C'),
-              onPressed: (){
-                Navigator.pop(context,Option.C);
-              },
-            ),
-          ],
-        );
-      }
-    );
+            children: <Widget>[
+              SimpleDialogOption(
+                child: Text('Option A'),
+                onPressed: () {
+                  Navigator.pop(context, Option.A);
+                },
+              ),
+              SimpleDialogOption(
+                child: Text('Option B'),
+                onPressed: () {
+                  debugPrint('onPressed1');
+                  Navigator.pop(context, Option.B);
+                },
+              ),
+              SimpleDialogOption(
+                child: Text('Option C'),
+                onPressed: () {
+                  Navigator.pop(context, Option.C);
+                },
+              ),
+            ],
+          );
+        });
     switch (option) {
       case Option.A:
         _choice = 'A';
@@ -57,30 +55,11 @@ class _DialogDemoState extends State<DialogDemo> {
         break;
       default:
     }
-    setState(() {
-      
-    });
-=======
-
-class DialogDemo extends StatefulWidget {
-  @override
-  _DialogDemoState createState() => new _DialogDemoState();
-}
-
-enum Option { A, B, C }
-
-class _DialogDemoState extends State<DialogDemo> {
-  String _content = 'Nothing';
-
-  @override
-  void initState() {
-    super.initState();
->>>>>>> 7ebd3ca1b0616671448a30f6f08b121f57167b08
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return Scaffold(
       appBar: AppBar(
         title: Text('Dialog'.toUpperCase()),
@@ -90,67 +69,80 @@ class _DialogDemoState extends State<DialogDemo> {
         onPressed: _openSimpleDialog,
       ),
       body: Container(
-      child: Center(
-        child: Text('Your choice is $_choice'),
-      ),
-    ),
-    );
-  }
-}
-=======
-    Future _showDialog() async {
-      final option = await showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return SimpleDialog(
-              title: Text('我是标题', style: TextStyle(fontSize: 16.0)),
-              children: <Widget>[
-                SimpleDialogOption(
-                  child: Text('Option A'),
-                  onPressed: () => Navigator.pop(context, Option.A),
-                ),
-                SimpleDialogOption(
-                  child: Text('Option B'),
-                  onPressed: () => Navigator.pop(context, Option.B),
-                ),
-                SimpleDialogOption(
-                  child: Text('Option C'),
-                  onPressed: () => Navigator.pop(context, Option.C),
-                ),
-              ],
-            );
-          });
-      switch (option) {
-        case Option.A:
-          _content = 'A';
-          break;
-        case Option.B:
-          _content = 'B';
-          break;
-        case Option.C:
-          _content = 'C';
-          break;
-        default:
-      }
-      setState(() {
-      });
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('dialog'.toUpperCase()),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.menu),
-        onPressed: _showDialog,
-      ),
-      body: Center(
-        child: Text(
-          'Your choice is $_content',
-          style: TextStyle(fontSize: 18),
+        child: Center(
+          child: Text('Your choice is $_choice'),
         ),
       ),
     );
   }
 }
->>>>>>> 7ebd3ca1b0616671448a30f6f08b121f57167b08
+
+enum Action {
+  Ok,
+  Cancel,
+}
+
+class AlertDialogDemo extends StatefulWidget {
+  @override
+  _AlertDialogDemoState createState() => _AlertDialogDemoState();
+}
+
+class _AlertDialogDemoState extends State<AlertDialogDemo> {
+  String _choice = 'Nothing';
+  Future _showAlert()async {
+     final result = await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('AlertDialog'),
+            content: Text('Are you sure about this?'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('ok'),
+                onPressed: (){
+                  Navigator.pop(context,Action.Ok);
+                },
+              ),
+              FlatButton(
+                child: Text('cancel'),
+                onPressed: (){
+                  Navigator.pop(context,Action.Cancel);
+                },
+              ),
+            ],
+          );
+        });
+        switch (result) {
+          case Action.Ok:
+            setState(() {
+              _choice = 'Ok';
+            });
+            break;
+          case Action.Cancel:
+            setState(() {
+              _choice = 'Cancel';
+            });
+            break;
+          default:
+            _choice = 'Nothing';
+        }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AlertDialog'.toUpperCase()),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('Your choice is: $_choice'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: _showAlert,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
           // home: NavigatorDemo(),
           initialRoute: '/',
           routes: {
-            '/':(BuildContext context) => DialogDemo(),
+            '/':(BuildContext context) => AlertDialogDemo(),
             '/about':(BuildContext context) => Page(title: 'About',),
           },
       //  home: Scaffold(
